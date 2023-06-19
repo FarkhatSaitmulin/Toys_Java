6d1b226311ba1d8c0d4a3bcbf627eb7e5b8a70eb
Frenk KAZ <farhats81@mail.ru>
2023-06-20 04:14:45 +0600

    Toys_Java

---
+++ src/Toy.java
@@ -0,0 +1,27 @@
+public class Toy {
+    private String id;
+    private String name;
+    private double weight;
+
+    public Toy(String id, String name, double weight) {
+        this.id = id;
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
---
+++ src/ToyQueue.java
@@ -0,0 +1,55 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Random;
+
+public class ToyQueue {
+    private PriorityQueue<Toy> queue;
+    private Random random;
+    private List<Toy> prizeToys;
+
+    public ToyQueue() {
+        queue = new PriorityQueue<>((t1, t2) -> (int) (t1.getWeight() - t2.getWeight()));
+        random = new Random();
+        prizeToys = new ArrayList<>();
+    }
+
+    public void addToy(Toy toy) {
+        queue.add(toy);
+    }
+
+    public void updateToyWeight(String id, double weight) {
+        Toy toy = queue.stream().filter(t -> t.getId().equals(id)).findFirst().orElse(null);
+        if (toy != null) {
+            toy.setWeight(weight);
+        }
+    }
+
+    private Toy chooseToy() {
+        double rand = random.nextDouble() * 100;
+        if (rand < 20) {
+            return queue.stream().filter(t -> t.getWeight() <= 1).findFirst().orElse(null);
+        } else if (rand < 40) {
+            return queue.stream().filter(t -> t.getWeight() > 1 && t.getWeight() <= 2).findFirst().orElse(null);
+        } else {
+            return queue.stream().filter(t -> t.getWeight() > 2 && t.getWeight() <= 6).findFirst().orElse(null);
+        }
+    }
+
+    public boolean givePrizeToy() {
+        if (!queue.isEmpty()) {
+            Toy prizeToy = chooseToy();
+            if (prizeToy != null) {
+                queue.remove(prizeToy);
+                prizeToys.add(prizeToy);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public List<Toy> getAllPrizeToys() {
+        return prizeToys;
+    }
+}
